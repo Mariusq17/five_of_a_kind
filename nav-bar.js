5c2f7e7bfb3361a503e99b66d176d8d876b4d46e
ignatmarius45 <ignatmarius45@gmail.com>
2022-11-27 00:02:16 +0200

    We organised the files

---
+++ nav-bar.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("burgur-menu");
+const navLinks = document.getElementById("links");
+
+let cnt = 0;
+
+btn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    if(cnt == 0) btn.style.backgroundImage = "url('images/close-menu.png')";
+        else btn.style.backgroundImage = "url('images/menu.png')";
+    cnt = 1 - cnt;
+})
